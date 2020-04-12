d3c9261dad72a2ce1a5f8ab207b82cf0b9e7eff5
chrstn.bwnkl <c.beiwinkel@students.uu.nl>
2020-04-12 18:33:27 +0200

    Added a basic Postgres connection

--- apiTask.js
+++ apiTask.js
@@ -1,11 +1,21 @@
 const CronJob = require('cron').CronJob;
 const axios = require('axios');
+const dotenv = require('dotenv');
+dotenv.config();
+const { Pool, Client } = require('pg');
+const conString = process.env.DATABASE_URL;
 
-var job = new CronJob('*/1 * * * *', function() {
-    axios.get('https://data.melbourne.vic.gov.au/resource/d6mv-s43h.json?$$app_token=f7dQeUuh1t2suGE3q3WMH8PPF&$limit=60')
-    .then(response => {
-        console.log(response.data);
-        counter +=1;
+var job = new CronJob('*/1 * * * *', async function() {
+    await axios.get('https://data.melbourne.vic.gov.au/resource/d6mv-s43h.json?$$app_token=f7dQeUuh1t2suGE3q3WMH8PPF&$limit=60')
+    .then( response => {
+        const pool = new Pool({
+            connectionString: conString
+        });
+        pool.query('SELECT * FROM test_table;'
+            , (err, res) => {
+            console.log(err, res, "Test")
+            pool.end()
+          });
   })
     .catch(error => {
         console.log(error);
@@ -13,7 +23,7 @@ var job = new CronJob('*/1 * * * *', function() {
 });
 
     module.exports = {
-        apiCall: function() {
+        apiUpdate: function() {
             job.start();
         }
     };    
--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const app = express();
-const apiTask = require('./apiTask')
-
+const apiTask = require('./apiTask');
 
 app.listen(3000, () => console.log('Listening at 3000'));
 app.use(express.static('public'));
 app.use(express.json({limit: '1mb'}));
 
 // Make scheduled API call
-apiTask.apiCall();
+apiTask.apiUpdate();
