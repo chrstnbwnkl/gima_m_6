ea1f5b4ba34aabcee92e1ff377d2fc21377f68d3
chrstn.bwnkl <c.beiwinkel@students.uu.nl>
2020-04-14 11:35:02 +0200

    Added Python test

--- apiTask.js
+++ apiTask.js
@@ -5,13 +5,15 @@ dotenv.config();
 const { Pool, Client } = require('pg');
 const conString = process.env.DATABASE_URL;
 
-var job = new CronJob('*/1 * * * *', async function() {
-    await axios.get('https://data.melbourne.vic.gov.au/resource/d6mv-s43h.json?$$app_token=f7dQeUuh1t2suGE3q3WMH8PPF&$limit=60')
+var job = new CronJob('*/15 * * * *', async function() {
+    await axios.get('https://data.melbourne.vic.gov.au/resource/d6mv-s43h.csv?$$app_token=f7dQeUuh1t2suGE3q3WMH8PPF&$limit=60')
     .then( response => {
         const pool = new Pool({
             connectionString: conString
         });
-        pool.query('SELECT * FROM test_table;'
+        pool.query(`
+        SELECT * FROM test_table;
+        `
             , (err, res) => {
             console.log(err, res, "Test")
             pool.end()
