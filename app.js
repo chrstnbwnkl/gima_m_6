b3b7d6015d4bf74ca5dc032d2af996523f150073
chrstn.bwnkl <c.beiwinkel@students.uu.nl>
2020-04-14 11:35:02 +0200

    Added Python test

--- app.js
+++ app.js
@@ -7,4 +7,11 @@ app.use(express.static('public'));
 app.use(express.json({limit: '1mb'}));
 
 // Make scheduled API call
-apiTask.apiUpdate();
+//apiTask.apiUpdate();
+
+// Testing Python child process
+const spawn = require("child_process").spawn;
+const pythonProcess = spawn('python',["./test.py", "Say hello ", "from "]);
+pythonProcess.stdout.on('data', (data) => {
+    console.log(data.toString());
+});
