b5392008c1b26b00a8f50f903c47614b6ba63ad7
chrstn.bwnkl <c.beiwinkel@students.uu.nl>
2020-04-17 19:33:56 +0200

    User input passed into python child process

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 const apiTask = require('./apiTask');
-const from = "688 Bourke Street, Melbourne, Australia";
-const to = "356 Collins Street, Melbourne, Australia";
+const cors = require('cors');
+var from;
+var to;
+
+app.use(cors());
 
 app.listen(3000, () => console.log('Listening at 3000'));
 app.use(express.static('public'));
@@ -11,9 +14,31 @@ app.use(express.json({limit: '1mb'}));
 //Make scheduled API call
 apiTask.apiUpdate();
 
-//Testing Python child process
-const spawn = require("child_process").spawn;
-const pythonProcess = spawn('python',["./test.py", from, to]);
-pythonProcess.stdout.on('data', (data) => {
-    console.log(data.toString());
+//Get user input
+app.get('/', (req, res) => {
+    res.json({
+        'message': 'Input received'
+    })
+});
+
+function isValid(x) {
+    return x.from && x.from.toString().trim() !== '' &&
+        x.to && x.to.toString().trim() !== '';
+}
+
+app.post('/navigate', (req, res) => {
+    if (isValid(req.body)) {
+        from = req.body.from.toString();
+        to = req.body.to.toString();
+        const spawn = require("child_process").spawn;
+        const pythonProcess = spawn('python',["./test.py", from, to]);
+        pythonProcess.stdout.on('data', (data) => {
+            console.log(data.toString());
+        });
+    } else {
+        res.status(422);
+        res.json({
+            message: 'Request can not be empty!'
+        })
+    }
 });
